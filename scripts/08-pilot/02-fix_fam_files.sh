5f07e5dcb64f849d53dc574e8cf54c9e02035a00
mdonertas <melike@ebi.ac.uk>
2018-08-28 16:23:56 +0100

    fix paths

--- scripts/08-pilot/02-fix_fam_files.sh
+++ scripts/08-pilot/02-fix_fam_files.sh
@@ -1,6 +1,6 @@
-mkdir -p /nfs/research1/thornton/ukbb_ageonset/data/raw/ukbb/fam4bolt
+mkdir -p ./data/processed/ukbb/gwas/fam4bolt/
 for i in {1..22}
 do
 echo ukb30688_cal_chr$i\_v2_s488346.fam
-awk '{print $1, $2, $3, $4, $5, 1}' /nfs/research1/thornton/ukbb_ageonset/data/raw/ukbb/fam/ukb30688_cal_chr$i\_v2_s488346.fam > /nfs/research1/thornton/ukbb_ageonset/data/raw/ukbb/fam4bolt/ukb30688_cal_chr$i\_v2_s488346.fam
+awk '{print $1, $2, $3, $4, $5, 1}' ./data/raw/ukbb/fam/ukb30688_cal_chr$i\_v2_s488346.fam > ./data/processed/ukbb/gwas/fam4bolt/ukb30688_cal_chr$i\_v2_s488346.fam
 done
