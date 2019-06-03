fd7f4898f7b7aee9a6842f14cf80f2234fa47bfd
melike <donertas.melike@gmail.com>
2019-06-03 12:53:56 +0100

    map all variants to uniprot sequences

---
+++ scripts/codingVariants/01-unimap.R
@@ -0,0 +1,87 @@
+library(tidyverse)
+library(GenomicRanges)
+library(Biostrings)
+# disID = 'a1521'
+# disdat = read_tsv(paste('./data/processed/ukbb/gwas/bolt/',disID,'.imp.stats',sep=''))
+# disdat = disdat %>%
+#   select(CHR,BP,SNP,ALLELE1,ALLELE0) %>%
+#   mutate(CHR = paste('chr',CHR,sep='')) %>%
+#   set_names(c('seqnames','start','ID','a1','a0')) %>%
+#   mutate(end = start)
+# disdat = makeGRangesFromDataFrame(disdat,keep.extra.columns = T)
+# disdat = disdat[which(nchar(disdat$a1) == 1 & nchar(disdat$a0) == 1),]
+# 
+# library(rtracklayer)
+# ch = import.chain("./data/raw/ucsc/liftover/hg19ToHg38.over.chain")
+# disdat = unlist(liftOver(disdat, ch))
+# print('hg19 to hg38 conversion is completed')
+# saveRDS(disdat,'./data/processed/ukbb/snpCoord_hg38.rds')
+# print('hg38 snp coordinates are saved')
+disdat = readRDS('./data/processed/ukbb/snpCoord_hg38.rds')
+print('disdat is read')
+unisx = grep('.rds',list.files('../melike/projects/shared_data/uniprot2genomicCoord/data/processed/uniprot_granges_hg38', full.names = T),v=T)
+unimap = lapply(unisx,function(unix){
+  print(unix)
+  uni = readRDS(unix)
+  uni = uni[which(uni$ismatch),]
+  uni = sort(uni)
+  fo = findOverlaps(disdat,uni)
+  xx = as.data.frame(disdat[queryHits(fo),]) %>%
+    mutate(snpPos = start) %>%
+    select(ID,snpPos,a0,a1)
+  xx = data.frame(xx,as.data.frame(uni[subjectHits(fo),])) %>%
+    select(-width)
+  xx
+})
+unimap = reshape2::melt(unimap,id.vars = colnames(unimap[[1]])) %>%
+  select(-L1) %>%
+  as.tibble()
+print('unimap is created')
+unimap2 = list()
+for( i in 1:nrow(unimap)){
+  print(i)
+  x = unimap[i,]
+  pos = ifelse(x$strand == '+', x$snpPos - x$start + 1, 3 - x$snpPos + x$start)
+  if(substr(x$genSeq,pos,pos) == x$a0) {
+    a1 = x$a1
+    x$a1 = x$a0
+    x$a0 = a1
+    ismatch_seq = (substr(x$genSeq, pos, pos) == x$a1)
+  } else if ( substr(x$genSeq, pos, pos) == x$a1){
+    ismatch_seq = T
+  } else if ( x$strand == '-'){
+    x$a0 = as.character(reverseComplement(DNAStringSet(x$a0)))
+    x$a1 = as.character(reverseComplement(DNAStringSet(x$a1)))
+    if(substr(x$genSeq,pos,pos) == x$a0) {
+      a1 = x$a1
+      x$a1 = x$a0
+      x$a0 = a1
+      ismatch_seq = (substr(x$genSeq, pos, pos) == x$a1)
+    } else if ( substr(x$genSeq, pos, pos) == x$a1){
+      ismatch_seq = T
+    } 
+  } else { 
+    ismatch_seq = F
+  }
+  genseq = tolower(x$genSeq)
+  genseq = strsplit(genseq, '')[[1]]
+  genseq2 = genseq
+  genseq2[pos] = x$a0
+  genseq[pos] = toupper(genseq[pos])
+  genseq = paste(genseq,collapse ='')
+  genseq2 = paste(genseq2,collapse ='')
+  genChange = paste(genseq,'/',genseq2,sep='')
+  newAA = unname(GENETIC_CODE[toupper(genseq2)])
+  type = ifelse(x$AA == newAA,'syn','missense')
+  unimap2[[i]] = as.data.frame(x) %>%
+    mutate(pos = pos, newGenSeq = toupper(genseq2), genChange =genChange, newAA = newAA,type = type, ismatch_seq =ismatch_seq)
+  saveRDS(unimap2,'./temp/unimap.rds')
+}
+print('unimap2 is created')
+unimap2 = reshape2::melt(unimap2,id.vars = colnames(unimap2[[1]])) %>%
+  select(-L1)
+print('unimap2 is melted')
+# system('mkdir ./data/processed/codingVariants')
+saveRDS(unimap2,'./data/processed/codingVariants/unimap.rds')
+print('unimap2 is saved under ./data/processed/codingVariants/unimap.rds')
+paste('finished')
