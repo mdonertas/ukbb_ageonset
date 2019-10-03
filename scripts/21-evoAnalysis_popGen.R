ec2de0185d3b03009f92bdaa1416997491371787
melike <donertas.melike@gmail.com>
2019-10-03 14:25:29 +0100

    read bgenfiles

--- scripts/21-evoAnalysis_popGen.R
+++ scripts/21-evoAnalysis_popGen.R
@@ -20,8 +20,16 @@ unqcl1 = setdiff(snplist$SNPs[[1]],union(snplist$SNPs[[2]],snplist$SNPs[[3]]))
 unqcl2 = setdiff(snplist$SNPs[[2]],union(snplist$SNPs[[1]],snplist$SNPs[[3]]))
 unqcl3 = setdiff(snplist$SNPs[[3]],union(snplist$SNPs[[1]],snplist$SNPs[[2]]))
 unqcl1 = tapply(unqcl1, sapply(strsplit(unqcl1,'_'),function(x)x[1]), c)
-unqcl1 = sort(names(unqcl1))[rank(as.character(1:22))]
+unqcl1 = sapply(unqcl1,function(x)x)
+unqcl1 = (unqcl1[sort(names(unqcl1))[rank(as.character(1:22))]])
 unqcl2 = tapply(unqcl2, sapply(strsplit(unqcl2,'_'),function(x)x[1]), c)
-unqcl2 = sort(names(unqcl2))[rank(as.character(1:22))]
+unqcl2 = sapply(unqcl2,function(x)x)
+unqcl2 = (unqcl2[sort(names(unqcl2))[rank(as.character(1:22))]])
 unqcl3 = tapply(unqcl3, sapply(strsplit(unqcl3,'_'),function(x)x[1]), c)
-unqcl3 = sort(names(unqcl3))[rank(as.character(1:22))]
+unqcl3 = sapply(unqcl3,function(x)x)
+unqcl3 = as.list(unqcl3[sort(names(unqcl3))[rank(as.character(1:22))]])
+library(bigsnpr)
+# system('mkdir -p ./data/processed/genotypes')
+x=snp_readBGEN(bgenfiles = paste("/nfs/research1/ukbb/500K_release_v3/imputed/EGAD00010001474/ukb_imp_chr",1:22,"_v3.bgen",sep=""), 
+               backingfile = './data/processed/genotypes/unqcl2', 
+               list_snp_id = unqcl2)
