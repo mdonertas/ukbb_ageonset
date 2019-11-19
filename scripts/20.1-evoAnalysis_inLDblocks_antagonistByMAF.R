5f218b1dda43a104fe402f49d5a016a229161b38
melike <donertas.melike@gmail.com>
2019-11-19 16:21:44 +0000

    update the filter cutoffs

--- scripts/20.1-evoAnalysis_inLDblocks_antagonistByMAF.R
+++ scripts/20.1-evoAnalysis_inLDblocks_antagonistByMAF.R
@@ -3,21 +3,21 @@ k=0
 signifSNPs = signifSNPs %>%
   mutate(snppos = paste(CHR,BP,sep='_'))
 mafxx = signifSNPs %>% 
-  mutate(betagr = c(1:10/10)[cut(abs(signifSNPs$BETA),breaks = quantile(abs(signifSNPs$BETA),probs = seq(0,1,length.out = 11)),include.lowest = T)])
+  mutate(betagr = c(1:20/20)[cut(abs(signifSNPs$BETA),breaks = quantile(abs(signifSNPs$BETA),probs = seq(0,1,length.out = 21)),include.lowest = T)])
 # mafxx = signifSNPs %>%
 #   mutate(MAF = ifelse(UKBB_RAF<=0.5,UKBB_RAF,1-UKBB_RAF)) %>% group_by(snppos) %>% summarise(MAF = min(MAF))
-for(mafx in c(10:1/10)){
+for(mafx in c(19:0/20)){
   k=k+1
   print(mafx)
   ld_cl1cl2_anta = apply(LDblocks,1,function(x){
     st=x['start']
     en=x['stop']
     ch=x['CHR']
-    snpsexc = unique(filter(mafxx,betagr>=mafx)$snppos)
+    snpsexc = unique(filter(mafxx,betagr<mafx)$snppos)
     xx = signifSNPs %>%
       select(-disID,-BETA,-P_BOLT_LMM_INF) %>%
       filter(CHR==ch & BP<=en & BP>=st) %>%
-      filter(snppos %in% snpsexc) %>%
+      filter(!snppos %in% snpsexc) %>%
       filter(cl1_cl2 == ' antagonist' & !grepl('agonist',cl1_cl3) & !grepl('agonist', cl2_cl3)) %>%
       unique() %>%
       mutate(cluster = factor(cluster)) %>% 
@@ -47,18 +47,18 @@ for(mafx in c(10:1/10)){
     filter(pop == 'UKBB') %>%
     ggplot(aes(y = RAF_median, x= cluster, fill = cluster)) +
     geom_violin()+
-    geom_sina(size=1,alpha=1)+
     geom_boxplot(width=0.1,fill='white') +
-    scale_fill_manual(values = ageonsetcolors) +
     stat_summary(fun.y = 'median', aes(label = round(..y..,2)), geom = "label",fill='white')  +
+    geom_jitter(size=1,alpha=0.6,width = 0.2)+
+    scale_fill_manual(values = ageonsetcolors) +
     guides(fill = F) +
     xlab('Age of onset cluster') + ylab('Risk Allele Frequency')+
     scale_y_continuous(limits=c(0,1.1),breaks = seq(0,1,by=0.2))+
     stat_compare_means(label = 'p.format',label.y=0.01,label.x = 0.5,hjust=0,size=6/pntnorm) +
     geom_text(data=unique(filter(ld_cl1cl2_anta,pop=='UKBB' & cluster==1)%>%select(cluster,nsum,nmean)),aes(x=0.5,label=paste('n_total=',nsum,', n_mean=',nmean,sep='')),y=1.1,size=6/pntnorm,hjust=0) +
-    ggtitle(paste(mafx*100 - 10,' - ',mafx*100,' % BETA',sep=''))
+    ggtitle(paste( '>= ',mafx*100,' % BETA',sep=''))
   print(antaplot[[k]])
 }
-p1=ggarrange(antaplot[[1]],antaplot[[2]],antaplot[[3]],antaplot[[4]],antaplot[[5]],antaplot[[6]],antaplot[[7]],antaplot[[8]],antaplot[[9]],antaplot[[10]],ncol=5,nrow=2,labels='auto')
-ggsave('./results/evoAnalysis/cl12_antagonist_maffiletrs.pdf',p1, width = 28,height = 14, units = 'cm',useDingbats=F)
-ggsave('./results/evoAnalysis/cl12_antagonist_maffiletrs.png',p1, width = 28,height = 14, units = 'cm')
+p1=ggarrange(plotlist = antaplot,ncol=5,nrow=4,labels='auto')
+ggsave('./results/evoAnalysis/cl12_antagonist_maffiletrs.pdf',p1, width = 28,height = 24, units = 'cm',useDingbats=F)
+ggsave('./results/evoAnalysis/cl12_antagonist_maffiletrs.png',p1, width = 28,height = 24, units = 'cm')
