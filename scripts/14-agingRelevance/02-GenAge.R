8df3594c7e1962ebc569a931b52f3774bb18efac
melike <donertas.melike@gmail.com>
2020-02-01 19:25:33 +0000

    update the genage analysis

--- scripts/14-agingRelevance/02-GenAge.R
+++ scripts/14-agingRelevance/02-GenAge.R
@@ -34,6 +34,7 @@ signifGenes <- signifGenes %>%
 rm(proxyGenes,eqtlGenes)
 
 ageonsetsum = signifGenes %>%
+  # filter(ageonset!='4') %>%
   group_by(geneid) %>%
   summarise(ageonsetclusters = paste(sort(unique(ageonset)),collapse = '-',sep='-'),
             numagecluster = length(unique(ageonset))) %>%
@@ -56,30 +57,26 @@ genedat = full_join(dissum,full_join(ageonsetsum,discatsum)) %>%
 
 rm(ageonsetsum,discatsum,dissum)
 
-cl1genes= (genedat %>%
-             filter(ageonsetclusters == '1') %>%
-             filter(numdiscat > 0))$geneid
+genegrs = genedat %>%
+  mutate(monedis = numdiseases>1,
+         monecat = numdiscat>1) %>%
+  group_by(ageonsetclusters,monedis,monecat) %>%
+  summarise(genelist = list(sort(unique(geneid))),
+            numgenes = length(unique(geneid))) %>%
+  filter(ageonsetclusters %in% c('1','2','3','1-2','1-2-3','1-3','2-3'))%>%
+  filter(monedis | monecat) %>%
+  mutate(name = paste('cl',ageonsetclusters,'_',c('all','multicat')[1+monecat],sep=''))
 
-cl2genes = (genedat %>%
-              filter(ageonsetclusters == '2') %>%
-              filter(numdiscat > 0))$geneid
+# genegrs$name = c('cl1_o_o','cl1_m_o','cl1_m_m','cl1-2_m_o','cl1-2_m_m','cl1-2-3_m_m','cl1-3_m_m','cl2_o_o','cl2_m_o','cl2_m_m','cl2-3_m_o','cl2-3_m_m','cl3_o_o','cl3_m_o','cl3_m_m')
+# genegrs = genegrs 
 
-cl3genes= (genedat %>%
-             filter(ageonsetclusters == '3') %>%
-             filter(numdiscat > 0))$geneid
+genegrs[1,]$genelist = list(unique(unlist(c(genegrs[1,]$genelist,genegrs[2,]$genelist))))
+genegrs[3,]$genelist = list(unique(unlist(c(genegrs[3,]$genelist,genegrs[4,]$genelist))))
+genegrs[7,]$genelist = list(unique(unlist(c(genegrs[7,]$genelist,genegrs[8,]$genelist))))
+genegrs[9,]$genelist = list(unique(unlist(c(genegrs[9,]$genelist,genegrs[10,]$genelist))))
+genegrs[11,]$genelist = list(unique(unlist(c(genegrs[11,]$genelist,genegrs[12,]$genelist))))
 
-cl1genes_h1cat = (genedat %>%
-                    filter(ageonsetclusters == '1') %>%
-                    filter(numdiscat > 1))$geneid
-
-cl2genes_h1cat = (genedat %>%
-                    filter(ageonsetclusters == '2') %>%
-                    filter(numdiscat > 1))$geneid
-
-cl3genes_h1cat = (genedat %>%
-                    filter(ageonsetclusters == '3') %>%
-                    filter(numdiscat > 1))$geneid
-
+saveRDS(genegrs,file='./data/processed/clustergenes.rds')
 ####
 
 # genagehuman = read_csv('../melike/projects/shared_data/GenAge/20190813/data/raw/genage_human.csv')
@@ -139,8 +136,7 @@ proxygenes = readRDS('./data/processed/caseControl/a1071/gwasRes_proxyGenes.rds'
 proxygenes = setdiff(unique(proxygenes$proxy_hgnc),NA)
 
 allgenes = unique(c(eqtlgenes,proxygenes))
-genegr = list(cl1genes,cl2genes,cl3genes,
-              cl1genes_h1cat,cl2genes_h1cat,cl3genes_h1cat)
+genegr = setNames(genegrs$genelist, genegrs$name)
 
 # resx = lapply(genegr,function(x){
 #   xx = t(sapply(aginggenes[c(1,2,7,8)],function(y){
@@ -168,43 +164,55 @@ permres = lapply(genegr,function(x){
   rownames(xx) = c('human','model organism','drug','senescence','all-combined')
   xx
 })
-names(permres) = paste(rep(1:3,2),rep(c('','_strict'),each=3),sep='')
+# names(permres) = paste(rep(1:3,2),rep(c('','_strict'),each=3),sep='')
 
-xx = reshape2::melt(permres) %>%
-  spread(Var2,value) %>%
-  separate(L1,into=c('cluster','type')) %>%
-  mutate(type=ifelse(is.na(type),'all',type)) %>%
+xx = reshape2::melt(permres) %>% 
+  spread(Var2,value) %>% 
+  # mutate(cluster = sapply(strsplit(L1,'_'),function(x)x[1])) %>%
+  # mutate(type = sapply(strsplit(L1,'_'),function(x)paste(x[2],x[3],sep="_"))) %>%
+  # separate(L1,into=c('cluster','more than one disease', 'more than one category'),sep = '_',remove = F) %>% 
+  # mutate(type=ifelse(is.na(type),'all',type)) %>%
+  separate(L1,into=c('cluster','type'),sep='_',remove=F) %>%
   rename(Aging = Var1) %>%
-  mutate(prop = log2(val/perm))
-oddsplot = xx %>% 
-  filter(type =='strict') %>%
-  ggplot() +
-  geom_bar(aes(x = cluster, y= prop, fill=cluster), stat='identity', position = 'dodge') +
-  facet_grid(~Aging) +
-  scale_fill_manual(values = ageonsetcolors) +
-  geom_label(aes(label = num, x= cluster),y=0.2)+
-  # ylim(-3.5,3.5) +
-  geom_text(data =filter(filter(xx,type=='strict'),p<=0.05),aes(x=cluster,y=-0.01,label=scales::pvalue(p,add_p = T,accuracy = 0.001)), angle=90,color='black',hjust=1) +
-  # geom_text(data =filter(xx,type=='strict'),aes(x=cluster,y=-0.01,label=scales::pvalue(p,add_p = T,accuracy = 0.001)), angle=90,color='black',hjust=1) +
-  xlab('Age of onset cluster') + ylab('Log2 Odds Ratio') +
-  guides(fill = F)
+  mutate(prop = log2(val/perm))%>%
+  left_join(rename(select(ungroup(genegrs),name,numgenes),L1=name))
+library(ggthemes)
 
-odds_all = xx %>% 
-  filter(type =='all') %>%
-  ggplot() +
-  geom_bar(aes(x = cluster, y= prop, fill=cluster), stat='identity', position = 'dodge') +
-  facet_grid(~Aging) +
-  scale_fill_manual(values = ageonsetcolors) +
-  geom_label(aes(label = num, x= cluster),y=0.1)+
-  # ylim(-1.5,1.5) +
-  geom_text(data =filter(filter(xx,type=='all'),p<=0.05),aes(x=cluster,y=-0.01,label=scales::pvalue(p,add_p = T,accuracy = 0.001)), angle=90,color='black',hjust=1) +
-  # geom_text(data =filter(xx,type=='all'),aes(x=cluster,y=-0.01,label=scales::pvalue(p,add_p = T,accuracy = 0.001)), angle=90,color='black',hjust=1) +
-  xlab('Age of onset cluster') + ylab('Log2 Odds Ratio') +
-  guides(fill = F)
+agingdb = setNames(c('GenAge-Human','GenAge-Model','DrugAge','CellAge','all'),c('human','model organism','drug','senescence','all-combined'))
+xx = xx %>% 
+  mutate(cluster=gsub('cl','',cluster),
+         type = c('Multidisease','Multicategory')[as.numeric(as.factor(type))]) %>%
+  mutate(Aging = agingdb[Aging]) %>%
+  mutate(cluster = factor(cluster, levels = c('1','2','3','1-2','1-3','2-3','1-2-3'))) %>%
+  mutate(type = factor(type,levels = c('Multidisease','Multicategory'))) %>%
+  mutate(prop = ifelse(prop==-Inf,NA,prop))
+oddsplot = ggplot(xx, aes(x=cluster, y = prop, color = Aging, size = p<=0.05)) +
+  geom_hline(yintercept = 0, color = 'darkred',linetype = 'dashed', size = 0.2, alpha = 0.5) +
+  geom_text(aes(label=paste(numgenes,'genes')), y = -1.6, color = 'gray25', size = 6/pntnorm,hjust=0,nudge_x = 0.4) +
+  geom_point(alpha=0.3,aes(shape=p<0.05)) + 
+  geom_text_repel(data = filter(xx,p<=0.05),aes(label = paste('(',num,')',sep='')), size = 6/pntnorm, box.padding = 0.25,min.segment.length = 0.25) +
+  scale_size_manual(values=setNames(c(0.5,2),c(F,T))) +
+  scale_shape_manual(values=setNames(c(4,19),c(F,T))) +
+  scale_color_gdocs() +
+  facet_wrap(~type, ncol = 2, nrow=1) +
+  coord_flip() +
+  xlab('') +
+  ylab('Log2 Enrichment Score') +
+  theme_pubr(base_size = 6) +
+  theme(panel.grid.major.y = element_line(color = 'gray90',linetype='dotted'),
+        legend.direction = 'horizontal', legend.position = 'bottom') +
+  guides(size = F, shape=F,
+         color = guide_legend('Database')) 
+ggsave(filename = './results/agingRelevance/genage3.pdf',oddsplot,units = 'cm',width = 16,height = 6,useDingbats=F)
+ggsave(filename = './results/agingRelevance/genage3.png',oddsplot,units = 'cm',width = 16,height = 6)
 
-oddsx = ggarrange(odds_all+ggtitle('Genes specific to one cluster'),oddsplot+ggtitle('Genes specific to one cluster and linked to multiple categories'),labels = 'auto',nrow=2)
-
-system('mkdir ./results/agingRelevance/')
+lapply(genegr,function(x){
+  xx = lapply(aginggenes[c(1,2,7,8,9)],function(y){
+    intersect(x,y)
+  })
+  names(xx) = c('human','model organism','drug','senescence','all-combined')
+  xx
+})
 ggsave('./results/agingRelevance/genage2.pdf',oddsx,units ='cm',width = 15,height = 20,useDingbats=F)
 ggsave('./results/agingRelevance/genage2.png',oddsx,units ='cm',width = 15,height = 20)
 
